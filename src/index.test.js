b3986ffff71ff9760b2961dadf32e8bb406abd9c
nadjibamar <hello@ebabel.eu>
2015-09-21 20:51:00 +0200

    test to be implemented

--- src/index.test.js
+++ src/index.test.js
@@ -3,21 +3,10 @@ var gaggregator = require('./index');
 
 describe('gaggregator', function() {
 
-    describe('all', function() {
+    describe('process', function() {
 
-        it('should return an array of strings', function() {
-            function isArrayOfStrings (array) {
-                return array.every(function (item) {
-                    return typeof item === 'string';
-                });
-            }
-
-            expect(starWars.all).to.satisfy(isArrayOfStrings);
-        });
-
-        it('should contain \'Luke Skywalker\'', function() {
-            expect(starWars.all).to.include('Luke Skywalker');
-        });
+        // implement a test that watches for custom event 'end_parsed' 
+        // and checks JSON has been created.
 
     });
 
